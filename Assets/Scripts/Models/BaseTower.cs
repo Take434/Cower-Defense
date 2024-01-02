fa325caf7401c2e9d97298ae90d9e727ea435355
Giesen, Jonas <jonas.giesen@hsrw.org>
2024-01-02 13:42:08 +0100

    added base classes for towers

---
+++ Assets/Scripts/Models/BaseTower.cs
@@ -0,0 +1,9 @@
+public class BaseTower
+{
+  public int Damage { get; set; }
+  public int Range { get; set; }
+  public int Cost { get; set; }
+  public int posX { get; set; }
+  public int posY { get; set; }
+  
+}
---
+++ Assets/Scripts/Models/ChickenTower.cs
@@ -0,0 +1,4 @@
+public class ChickenTower : BaseTower  {
+  public Resources Resource { get; set;}
+  public int ResourceYield { get; set;}
+}
