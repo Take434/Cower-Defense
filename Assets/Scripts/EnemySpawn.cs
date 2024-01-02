addb6cc5630e265c72d96de1c2b21229eccc63cc
Take434 <timh4376@gmail.com>
2024-01-02 20:23:16 +0100

    random enemy spawn

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -1,19 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab;
-    int a = 0;
+    private List<GameObject> possibleEnemies;
+    float a = 0.5f;
+    void Start()
+    {
+        possibleEnemies = GameObject.FindGameObjectsWithTag("enemy").ToList();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(a < 3) {
+        Debug.Log(Time.time);
+        if(Time.time >= a) {
+            GameObject enemyPrefab = possibleEnemies[UnityEngine.Random.Range(0, possibleEnemies.Count)];
             GameObject instance = Instantiate(enemyPrefab, transform.position, Quaternion.Euler(90, 0, 0));
-            instance.GetComponent<EnemyMovement>().moveSpeed = Random.Range(1, 5);
-            a++;
+            instance.GetComponent<EnemyMovement>().moveSpeed = UnityEngine.Random.Range(1, 5);
+            a += UnityEngine.Random.Range(2f, 5f);
         }
     }
 }
