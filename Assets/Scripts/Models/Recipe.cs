b642cfeb4b1fe586caecc13f4049112cef003bad
Jonas <j.giesen@milestone-consult.de>
2024-01-23 12:14:08 +0100

    added most of the ui back in

--- Assets/Scripts/Models/Recipe.cs
+++ Assets/Scripts/Models/Recipe.cs
@@ -1,4 +1,12 @@
+using System.Collections.Generic;
+
 public class Recipe {
-  public Recipe(map<string, int> ingredients, int income) {
+  public string name;
+  public KeyValuePair<string, int> ingredients;
+  public int income;
+  public Recipe(string name, KeyValuePair<string, int> ingredients, int income) {
+    this.name = name;
+    this.ingredients = ingredients;
+    this.income = income;
   }
 }
